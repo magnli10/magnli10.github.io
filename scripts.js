a912bd942fe2455e160b2511544e3db3a627854a
Magnus Lien <lien_magnus@outlook.com>
2016-10-24 23:39:11 +0200

    Add scroll into view scripts

---
+++ scripts.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let currentStop = null;
+
+document.addEventListener("click", function () {
+    let scrollstops = Array.from(document.getElementsByClassName('scrollstop'));
+
+    if(!currentStop || currentStop === scrollstops[scrollstops.length - 1]) {
+        currentStop = scrollstops[0];
+    }else{
+        currentStop = scrollstops[scrollstops.indexOf(currentStop) + 1];
+    }
+
+    currentStop.scrollIntoView({ block: true, behavior: "smooth"});
+});
